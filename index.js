eb9ff01680ac91b26b0e3a5ddb85266730b35801
VictoriaRial20 <viqirial@gmail.com>
2023-01-28 18:08:28 -0800

    Proyect base and folders

---
+++ index.js
@@ -0,0 +1,3 @@
+import App from "./src/index";
+import { registerRootComponent } from "expo";
+registerRootComponent(App);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { styles } from './styles';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
+
