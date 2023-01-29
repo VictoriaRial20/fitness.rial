6524c1bb10125f44d6db45f63e250d2923f91068
VictoriaRial20 <viqirial@gmail.com>
2023-01-28 18:29:30 -0800

    Header

---
+++ src/components/header/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#A1C6EA',
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#04080F',
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { Text, View } from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
+import { Header } from './components/index';
+import { View } from 'react-native';
 import { styles } from './styles';
-
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Fitness Group"/>
     </View>
   );
 }
