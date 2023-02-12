41544cb079c59e2b908be75a371c56573328f6ab
VictoriaRial20 <viqirial@gmail.com>
2023-02-12 14:55:33 -0800

    Create data categories and muscule

---
+++ src/constants/data/categories.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = [
+    {
+        id: 1,
+        title: 'TREN SUPERIOR',
+        color: '#78CDD7',
+    },
+    {
+        id: 2,
+        title: 'TREN INFERIOR',
+        color: '#44A1A0',
+    },
+];
